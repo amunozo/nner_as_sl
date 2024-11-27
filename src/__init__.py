4020ea05f9a6565527128e67ad4defefe8e7e33d
amunozo <alberto.munoz.ortiz@udc.es>
2024-11-27 16:18:58 +0100

    refactor imports in evaluation module for clarity

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 from .data import utils
 from .machamp import configs
-from .evaluation import eval_caio, evaluator
+from .evaluation import utils, evaluator
--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,2 +1,2 @@
 from .evaluator import *
-from .eval_caio import *
+from .utils import *
