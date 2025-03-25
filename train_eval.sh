9602d3040873a31f2f22798bdff0ea6e815a3839
amunozo <alberto.munoz.ortiz@udc.es>
2025-03-25 15:50:40 +0100

    Refactor training and evaluation script to simplify dataset and encoder handling

--- train_eval.sh
+++ train_eval.sh
@@ -1,19 +1,22 @@
-datasets=("genia" "ace2004" "ace2005" "nne")
-encoders=("bert-base-uncased" "answerdotai/ModernBERT-base")
-n_seeds=5
+datasets=("genia") # "ace2004" "ace2005" "nne")
+encoders=("answerdotai/ModernBERT-base") # bert-base-uncased
+n_seeds=1
 
 for dataset in "${datasets[@]}"
 do
-    python train.py --dataset "$dataset" --encoder bert-base-uncased --encoding ABS --device 0 --n_seeds $n_seeds &
-    python train.py --dataset "$dataset" --encoder bert-base-uncased --encoding REL --device 1 --n_seeds $n_seeds &
-    wait 
-    python train.py --dataset "$dataset" --encoder bert-base-uncased --encoding JUX --device 0 --n_seeds $n_seeds &
-    python train.py --dataset "$dataset" --encoder bert-base-uncased --encoding DYN --device 1 --n_seeds $n_seeds &
-    wait
-    python evaluate.py --dataset "$dataset" --encoder bert-base-uncased --encoding ABS --device 0 &
-    python evaluate.py --dataset "$dataset" --encoder bert-base-uncased --encoding REL --device 1 &
-    wait
-    python evaluate.py --dataset "$dataset" --encoder bert-base-uncased --encoding JUX --device 0 &
-    python evaluate.py --dataset "$dataset" --encoder bert-base-uncased --encoding DYN --device 1 &
-    wait
+    for encoder in "${encoders[@]}"
+    do
+        python train.py --dataset "$dataset" --encoder "$encoder" --encoding ABS --device 0 --n_seeds $n_seeds &
+        #python train.py --dataset "$dataset" --encoder "$encoder" --encoding REL --device 1 --n_seeds $n_seeds &
+        #wait 
+        #python train.py --dataset "$dataset" --encoder "$encoder" --encoding JUX --device 0 --n_seeds $n_seeds &
+        #python train.py --dataset "$dataset" --encoder "$encoder" --encoding DYN --device 1 --n_seeds $n_seeds &
+        #wait
+        python evaluate.py --dataset "$dataset" --encoder "$encoder" --encoding ABS --device 0 &
+        #python evaluate.py --dataset "$dataset" --encoder "$encoder" --encoding REL --device 1 &
+        #wait
+        #python evaluate.py --dataset "$dataset" --encoder "$encoder" --encoding JUX --device 0 &
+        #python evaluate.py --dataset "$dataset" --encoder "$encoder" --encoding DYN --device 1 &
+        3wait
+    done
 done
